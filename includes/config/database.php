5a84244e824304a10b646f56dec509b69ad05488
IsaiasCastilloJimenez <112017999+IsaiasCastilloJimenez@users.noreply.github.com>
2022-09-04 18:51:53 -0500

    Update database.php

--- includes/config/database.php
+++ includes/config/database.php
@@ -7,14 +7,7 @@ function conectarDB() : mysqli {
     $_ENV['DB_PASS'] ?? '',
     $_ENV['DB_BD']
       );
- 
-  // $db = new mysqli(
-    //   $_ENV['DB_HOST'],
-    //   $_ENV['DB_USER'],
-    //   $_ENV['DB_PASS'] ?? '',
-    //   $_ENV['DB_BD']
-    // );
-    $db->set_charset('utf8');
+    $db->set_charset("utf8");
   
     if(!$db) {
         echo "Error, No se pudo conectar";
